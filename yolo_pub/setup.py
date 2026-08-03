e2961ae92fd1fa2980775432911ed21177895430
Chan Hyeok Kim <151709991+Chan-Hyeok-Kim-git@users.noreply.github.com>
2026-08-03 20:09:39 +0900

    카메라로부터 rgb/depth 수신해 포인트 클라우드 발행

---
+++ yolo_pub/setup.py
@@ -0,0 +1,32 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'yolo_pub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/resource', glob('resource/*.pt')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rokey',
+    maintainer_email='habinkang.k@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'yolo_pub = yolo_pub.yolo_pub:main'
+        ],
+    },
+)
